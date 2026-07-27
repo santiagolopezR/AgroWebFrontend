53e302dac9b256b5b9c3447d6c6833b187e4fac1
santiagolopezR <198632381+santiagolopezR@users.noreply.github.com>
2026-07-27 08:41:16 -0500

    Update Zafras.jsx

--- src/Zafras.jsx
+++ src/Zafras.jsx
@@ -55,6 +55,8 @@ export default function Zafras() {
 
   const fetchCultivos = async () => {
     const { data } = await supabase.from('api_cultivocatalogo').select('id, nombre')
+    setCultivos(data || [])
+  }
 
   const fetchZafras = async () => {
     const { data } = await supabase.from('api_zafra').select('*').eq('lote_id', parseInt(loteSeleccionado))
